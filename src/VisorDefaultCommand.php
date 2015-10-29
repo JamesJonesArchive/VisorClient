736d7125d7ce611c884e8f4cc3388444174c98ab
James Jones <jamjon3@gmail.com>
2015-10-29 10:09:55 -0400

    Initial commit

---
+++ src/VisorDefaultCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Copyright 2015 University of South Florida
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace USF\IdM;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+use \USF\IdM\UsfConfig;
+/**
+ * Description of VisorDefaultCommand
+ *
+ * @author james
+ */
+class VisorDefaultCommand extends Command {
+    private $config;
+    protected function configure() {
+        $this->setName("visor:find")
+            ->setDescription("Retrieves Visor data for passed identifier")
+            ->addArgument('id',InputArgument::REQUIRED,'What is the identifier to look up?')
+            ->addOption('track',null,InputOption::VALUE_NONE,'If set, accounts will be tracked against existing to remove missing ones')
+            ->addOption('type',null,InputOption::VALUE_REQUIRED,'What is the system type (used in tracking)?',false)
+            ->setHelp("Usage: <info>php console.php visor:find <env></info>");
+        // Access configuration values from default location (/usr/local/etc/idm_config)
+        $this->config = new UsfConfig();
+    }
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $id = $input->getArgument('id');
+        $usfVisorAPI = new \USF\IdM\USFVisorAPI($this->config->visorConfig);        
+        $output->writeln($usfVisorAPI->getVisor($id)->encode());
+    }
+}
